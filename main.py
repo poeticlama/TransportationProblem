4e2d73f6963e2d73ca4d5dfd095be5e2303c9c2a
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2024-11-08 18:02:45 +0300

    added first version of vogel's approximation method

--- main.py
+++ main.py
@@ -31,8 +31,69 @@ def north_west_corner(S: list[int], C: list[list[int]], D: list[int]) -> (list[l
     return solution, z
 
 
-def vogel(S: list[int], C: list[list[int]], D: list[int]):
-    return
+def vogel(S: list[int], C: list[list[int]], D: list[int]) -> (list[list[int]], int):
+    D_copy = D.copy()
+    S_copy = S.copy()
+    C_copy = C.copy()
+    z = 0
+    solution = [[0 for _ in range(len(C[0]))] for _ in range(len(C))]
+    columns = list()
+    for j in range(len(C[0])):
+        columns.append([C[i][j] for i in range(len(C))])
+    row_difference = [0 for _ in range(len(S))]
+    column_difference = [0 for _ in range(len(D))]
+    while sum(S_copy) > 0:
+        for i in range(len(D)):
+            column_sorted = sorted(columns[i])
+            if column_sorted[1] != 10**5:
+                column_difference[i] = column_sorted[1] - column_sorted[0]
+            else:
+                column_difference[i] = 0
+        for i in range(len(S)):
+            row_sorted = sorted(C_copy[i])
+            if row_sorted[1] != 10**5:
+                row_difference[i] = row_sorted[1] - row_sorted[0]
+            else:
+                row_difference[i] = 0
+        max_row = max(row_difference)
+        max_column = max(column_difference)
+        if max_row >= max_column:
+            ind_diff = row_difference.index(max_row)
+            chosen_el = min(C_copy[ind_diff])
+            elem_indexes = (ind_diff, C_copy[ind_diff].index(chosen_el))
+            if S_copy[elem_indexes[0]] >= D_copy[elem_indexes[1]]:
+                solution[elem_indexes[0]][elem_indexes[1]] = D_copy[elem_indexes[1]]
+                z += solution[elem_indexes[0]][elem_indexes[1]] * C_copy[elem_indexes[0]][elem_indexes[1]]
+                S_copy[elem_indexes[0]] -= D_copy[elem_indexes[1]]
+                D_copy[elem_indexes[1]] -= D_copy[elem_indexes[1]]
+                C_copy[elem_indexes[0]][elem_indexes[1]] = 10**5
+                columns[elem_indexes[1]][elem_indexes[0]] = 10**5
+            elif S_copy[elem_indexes[0]] < D_copy[elem_indexes[1]]:
+                solution[elem_indexes[0]][elem_indexes[1]] = S_copy[elem_indexes[0]]
+                z += solution[elem_indexes[0]][elem_indexes[1]] * C_copy[elem_indexes[0]][elem_indexes[1]]
+                D_copy[elem_indexes[1]] -= S_copy[elem_indexes[0]]
+                S_copy[elem_indexes[0]] -= S_copy[elem_indexes[0]]
+                C_copy[elem_indexes[0]][elem_indexes[1]] = 10 ** 5
+                columns[elem_indexes[1]][elem_indexes[0]] = 10 ** 5
+        elif max_column > max_row:
+            ind_diff = column_difference.index(max_column)
+            chosen_el = min(columns[ind_diff])
+            elem_indexes = (ind_diff, columns[ind_diff].index(chosen_el))
+            if S_copy[elem_indexes[1]] >= D_copy[elem_indexes[0]]:
+                solution[elem_indexes[1]][elem_indexes[0]] = D_copy[elem_indexes[0]]
+                z += solution[elem_indexes[1]][elem_indexes[0]] * columns[elem_indexes[0]][elem_indexes[1]]
+                S_copy[elem_indexes[1]] -= D_copy[elem_indexes[0]]
+                D_copy[elem_indexes[0]] -= D_copy[elem_indexes[0]]
+                C_copy[elem_indexes[1]][elem_indexes[0]] = 10 ** 5
+                columns[elem_indexes[0]][elem_indexes[1]] = 10 ** 5
+            elif S_copy[elem_indexes[1]] < D_copy[elem_indexes[0]]:
+                solution[elem_indexes[1]][elem_indexes[0]] = S_copy[elem_indexes[1]]
+                z += solution[elem_indexes[1]][elem_indexes[0]] * columns[elem_indexes[0]][elem_indexes[1]]
+                D_copy[elem_indexes[0]] -= S_copy[elem_indexes[1]]
+                S_copy[elem_indexes[1]] -= S_copy[elem_indexes[1]]
+                C_copy[elem_indexes[1]][elem_indexes[0]] = 10 ** 5
+                columns[elem_indexes[0]][elem_indexes[1]] = 10 ** 5
+    return solution, z
 
 
 def russell(S: list[int], C: list[list[int]], D: list[int]):
@@ -41,11 +102,18 @@ def russell(S: list[int], C: list[list[int]], D: list[int]):
 
 def main():
     S, C, D = input_data()
-    north_west_solution, z = north_west_corner(S, C, D)
+    if sum(S) != sum(D):
+        print('The problem is not balanced!')
+    solution, z = north_west_corner(S, C, D)
     print('Initial basic feasible solution by using North-West corner method:')
-    for vector in north_west_solution:
+    for vector in solution:
         print(vector)
     print('Answer:', z)
-
+    print()
+    solution, z = vogel(S, C, D)
+    print("Initial basic feasible solution by using Vogel's approximation method:")
+    for vector in solution:
+        print(vector)
+    print('Answer:', z)
 
 main()
